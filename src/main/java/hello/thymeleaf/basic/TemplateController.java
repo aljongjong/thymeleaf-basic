5dedc31506bd8fcb8e2d217ba8907da5fc222a45
이종훈 <aljongjong@gmail.com>
2025-09-13 16:30:36 +0900

    feat: 타임리프 템플릿 레이아웃 head

--- src/main/java/hello/thymeleaf/basic/TemplateController.java
+++ src/main/java/hello/thymeleaf/basic/TemplateController.java
@@ -1,7 +1,6 @@
 package hello.thymeleaf.basic;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -12,5 +11,10 @@ public class TemplateController {
     @GetMapping("/fragment")
     public String template() {
         return "template/fragment/fragmentMain";
+    }
+
+    @GetMapping("/layout")
+    public String layout() {
+        return "template/layout/layoutMain";
     }
 }
